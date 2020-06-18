2ac04b97e813fd1f2db1b08f03bd9776d72990af
Emiliano <emilianofuchs@gmail.com>
2020-06-18 01:53:25 -0300

    banco finalizado

--- banco.sql
+++ banco.sql
@@ -1,3 +1,36 @@
+CREATE TABLE endereco
+(
+    idEndereco      INT         NOT NULL AUTO_INCREMENT,
+    estado          VARCHAR(2)  NOT NULL,
+    cidade          VARCHAR(20) NOT NULL,
+    bairro          VARCHAR(20) NOT NULL,
+    rua             VARCHAR(30) NOT NULL,
+    numero          INT(7)      NOT NULL,
+    complemento     VARCHAR(20),
+
+    PRIMARY KEY(idEndereco)
+);
+
+CREATE TABLE escola
+(
+    idEscola        INT         NOT NULL AUTO_INCREMENT,
+    nomeEscola      VARCHAR(30) NOT NULL,
+    bairroEscola    VARCHAR(20) NOT NULL,
+    anoLetivo       VARCHAR(20) NOT NULL,
+    turma           VARCHAR(10) NOT NULL,
+    perfilEscola    VARCHAR(10) NOT NULL,
+
+    PRIMARY KEY(idEscola)
+);
+
+CREATE TABLE resultado
+(
+    idResultado     INT         NOT NULL AUTO_INCREMENT,
+    resultado       VARCHAR(20) NOT NULL,
+
+    PRIMARY KEY (idResultado)
+);
+
 CREATE TABLE pessoa
 (
     idPessoa                INT         NOT NULL AUTO_INCREMENT,
@@ -15,39 +48,4 @@ CREATE TABLE pessoa
     FOREIGN KEY (idEndereco_endereco) REFERENCES endereco(idEndereco),
     FOREIGN KEY (idEscola_escola) REFERENCES escola(idEscola),
     FOREIGN KEY (idResultuado_resultado) REFERENCES resultado(idResultado)
-);
-
-CREATE TABLE endereco
-(
-    idEndereco      INT NOT NULL AUTO_INCREMENT,
-    estado          VARCHAR(2)  NOT NULL,
-    cidade          VARCHAR(20) NOT NULL,
-    bairro          VARCHAR(20) NOT NULL,
-    rua             VARCHAR(30) NOT NULL,
-    numero          INT(7)      NOT NULL,
-    complemento     VARCHAR(20),
-
-    PRIMARY KEY(idEndereco)
-);
-
-CREATE TABLE escola
-(
-    idEscola       INT         NOT NULL AUTO_INCREMENT,
-    escola          VARCHAR(30) NOT NULL,
-    bairroEscola    VARCHAR(20) NOT NULL,
-    ano             VARCHAR(20) NOT NULL,
-    turma           VARCHAR(10) NOT NULL,
-    perfilEscola    VARCHAR(10),
-
-    PRIMARY KEY(idEscola)
-);
-
-CREATE TABLE resultado
-(
-    idResultado    INT         NOT NULL AUTO_INCREMENT,
-    resultado       VARCHAR(20) NOT NULL,
-    idPessoa_pessoa INT         NOT NULL,
-
-    PRIMARY KEY (idResultado),
-    FOREIGN KEY (idPessoa_pessoa) REFERENCES pessoa(idPessoa)
 );
